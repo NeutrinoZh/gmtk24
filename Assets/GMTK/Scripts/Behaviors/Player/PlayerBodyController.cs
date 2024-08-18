f7d695457d603a91450e91e3ed33c0a2bdce6316
Sanyo4ek <oleksandr.panchenko373@gmail.com>
2024-08-18 14:13:16 +0300

    fixed speedScale

--- Assets/GMTK/Scripts/Behaviors/Player/PlayerBodyController.cs
+++ Assets/GMTK/Scripts/Behaviors/Player/PlayerBodyController.cs
@@ -21,7 +21,7 @@ namespace GMTK
             var inputValue = _input.Actions.PlayerBody.Move.ReadValue<Vector2>();
             var backMovement = inputValue.y < 0 ? 0.05f : 1f;
 
-            _body.Move(inputValue.y * _playerStats.Speed * backMovement);
+            _body.Move(inputValue.y * _playerStats.SpeedScale * backMovement);
             _body.Rotate(inputValue.x);
         }
     }
